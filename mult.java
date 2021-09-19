31bacc419507ea9057b703b33b5f1909b269e749
jaimeen10 <69434277+jaimeen10@users.noreply.github.com>
2021-09-19 20:37:15 +0530

    'first'

---
+++ mult.java
@@ -0,0 +1,10 @@
+public class mult
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        a=Integer.parseInt(args[0]);
+        b=Integer.parseInt(args[1]);
+        System.out.println("Multiplication is:" +(a*b));
+    }
+}
